f1283825e3810b13fe1314b4e93eb7733d32772c
Kultán Juraj <kultanj1@gmail.com>
2020-10-21 13:22:28 +0200

    Tour of heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloAppslabComponent } from './hello-appslab/hello-appslab.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloAppslabComponent
   ],
   imports: [
     BrowserModule,
