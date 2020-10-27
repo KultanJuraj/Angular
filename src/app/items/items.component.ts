0f612471aece07efb735854500ff37967016482b
Kultán Juraj <kultanj1@gmail.com>
2020-10-27 18:03:13 +0100

    Tour of heroes - rozsirene

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Item} from '../Item';
+import {Item} from '../item';
 import { ITEMS } from '../mock-items';
 @Component({
   selector: 'app-items',
