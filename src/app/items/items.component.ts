05b454d9043a267e83b8e69e6c9e078d0d2d7bb6
Kultán Juraj <kultanj1@gmail.com>
2020-10-27 16:27:51 +0100

    Tour of heroes-zoznam

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import {Item} from '../Item';
-
+import { ITEMS } from '../mock-items';
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.css']
 })
 export class ItemsComponent implements OnInit {
-  items:Item = {
-    id:1,
-    name:"Meat",
-    price: "25"
-  };
+  items = ITEMS;
+  selectedItem: Item; 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSelect(item: Item): void {
+    this.selectedItem = item;
   }
 
 }
